8b3f2171c77a74cf71603e112756b066d4dac3fe
hello2727 <jihye7369@gmail.com>
2021-01-29 02:47:01 +0900

    프로젝트 시작

---
+++ app/src/main/java/com/example/android/camera_filter/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.camera_filter.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.camera_filter.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
