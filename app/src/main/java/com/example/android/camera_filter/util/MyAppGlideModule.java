9378a01d112178990b29a53d5a3a8cf1f96f1701
hello2727 <jihye7369@gmail.com>
2021-01-31 14:32:02 +0900

    이미지캡처 후 캡처된 사진 미리보기 기능 구현

---
+++ app/src/main/java/com/example/android/camera_filter/util/MyAppGlideModule.java
@@ -0,0 +1,7 @@
+package com.example.android.camera_filter.util;
+
+import com.bumptech.glide.module.AppGlideModule;
+
+public class MyAppGlideModule extends AppGlideModule {
+
+}
--- app/src/main/java/com/example/android/camera_filter/view/MainActivity.java
+++ app/src/main/java/com/example/android/camera_filter/view/MainActivity.java
@@ -5,20 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Size;
+import android.view.OrientationEventListener;
+import android.view.Surface;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 import com.example.android.camera_filter.R;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.pedro.library.AutoPermissions;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
     ProcessCameraProvider cameraProvider;
     CameraSelector cameraSelector;
     Camera camera;
+    ImageAnalysis imageAnalysis;
     ImageButton btn_capture;
     ImageCapture imageCapture;
 
@@ -50,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(tb);
 
         //카메라 미리보기 설정
-        setCamera();
+        setupCamera();
         //카메라 권한체크
         AutoPermissions.Companion.loadAllPermissions(this, 321);
 
@@ -59,9 +80,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 capture();
-
-                previewView.setVisibility(View.GONE);
-                iv_captured.setVisibility(View.VISIBLE);
             }
         });
     }
@@ -69,16 +87,40 @@ public class MainActivity extends AppCompatActivity {
     void capture(){
         imageCapture.takePicture(ContextCompat.getMainExecutor(this), new ImageCapture.OnImageCapturedCallback() {
             @Override
-            public void onCaptureSuccess(@NonNull ImageProxy image) {
-                super.onCaptureSuccess(image);
+            public void onCaptureSuccess(@NonNull ImageProxy imageProxy) {
+                super.onCaptureSuccess(imageProxy);
 
-                iv_captured.setImageResource(R.drawable.ic_launcher_foreground);
+                Matrix roatateMatrix = new Matrix();
+                roatateMatrix.postRotate(90);
+
+                //비트맵으로 변환 후 회전
+                Bitmap originImg = imageProxyToBitmap(imageProxy);
+                Bitmap rotateImg = Bitmap.createBitmap(originImg, 0, 0, originImg.getWidth(), originImg.getHeight(), roatateMatrix, false);
+
+                Glide.with(getApplicationContext())
+                     .load(rotateImg)
+                     .into(iv_captured);
+                previewView.setVisibility(View.GONE);
+                iv_captured.setVisibility(View.VISIBLE);
+                
+                imageProxy.close();
             }
+
         });
     }
 
+    //이미지프록시를 비트맵으로 변환
+    Bitmap imageProxyToBitmap(ImageProxy imageProxy){
+        ByteBuffer byteBuffer = imageProxy.getPlanes()[0].getBuffer();
+        byteBuffer.rewind();
+        byte[] bytes = new byte[byteBuffer.capacity()];
+        byteBuffer.get(bytes);
+        byte[] clonedBytes = bytes.clone();
+        return BitmapFactory.decodeByteArray(clonedBytes, 0, clonedBytes.length);
+    }
+
     // CameraProvider 사용 가능 여부 확인
-    void setCamera(){
+    void setupCamera(){
         //CameraProvider를 요청
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         //CameraProvider를 요청한 후, 뷰를 만들 때 초기화에 성공했는지 확인
@@ -93,23 +135,43 @@ public class MainActivity extends AppCompatActivity {
     }
     // 카메라 선택 및 수명 주기와 사용 사례 결합
     void bindPreview(@NonNull ProcessCameraProvider cameraProvider){
-        // 1. Preview를 만든다.
+        // Preview를 만든다.
         preview = new Preview.Builder()
                 .build();
 
-        // 2. 원하는 카메라 LensFacing 옵션을 지정한다.
+        // 원하는 카메라 LensFacing 옵션을 지정한다.
         cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                 .build();
 
-        // 3. 선택한 카메라와 사용 사례를 수명 주기에 결합한다.
+        // 선택한 카메라와 사용 사례를 수명 주기에 결합한다.
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
-        // 4. 사진을 찍기 위한 기본적인 컨트롤 제공
-        imageCapture = new ImageCapture.Builder()
-                .build();
+        // 사진을 찍기 위한 기본적인 컨트롤 제공
+        imageCapture = new ImageCapture.Builder().build();
 
-        // 5. preview를 previewView에 연결한다.
+        //회전
+        OrientationEventListener orientationEventListener = new OrientationEventListener(this) {
+            @Override
+            public void onOrientationChanged(int orientation) {
+                int rotation;
+
+                if(orientation >= 45 && orientation < 135){
+                    rotation = Surface.ROTATION_270;
+                }else if(orientation >= 135 && orientation < 225){
+                    rotation = Surface.ROTATION_180;
+                }else if(orientation >= 225 && orientation < 315){
+                    rotation = Surface.ROTATION_90;
+                }else{
+                    rotation = Surface.ROTATION_0;
+                }
+
+                imageCapture.setTargetRotation(rotation);
+            }
+        };
+        orientationEventListener.enable();
+
+        // preview를 previewView에 연결한다.
         camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
     }
 
