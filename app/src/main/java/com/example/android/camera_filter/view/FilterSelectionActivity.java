d07b379960a844a6f040ec635cbd9381152ed50e
hello2727 <jihye7369@gmail.com>
2021-01-31 21:40:23 +0900

    필터선택 액티비티 생성

---
+++ app/src/main/java/com/example/android/camera_filter/view/FilterSelectionActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.camera_filter.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.camera_filter.R;
+
+public class FilterSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter_selection);
+    }
+}
